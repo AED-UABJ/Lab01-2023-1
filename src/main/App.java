5f80643018955a4ef2046911a735ea1091027b10
Gladistony Silva Lins <bistone3000@hotmail.com>
2023-10-19 16:43:23 -0300

    atividade 1 e 2

--- src/main/App.java
+++ src/main/App.java
@@ -1,11 +1,28 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        return encontre(arrayOrdenado, num,0, arrayOrdenado.length-1);
+    }
+    public static int encontre(int[] arrayOrdenado, int num, int inicio, int fim){
+        if (inicio > fim) return 0;
+        int m = (inicio + fim) / 2;
+        if (arrayOrdenado[m] == num){
+            return 1;
+        } else if (arrayOrdenado[m] < num){
+            return encontre(arrayOrdenado, num, m+1, fim);
+        } else {
+            return encontre(arrayOrdenado, num, inicio, m-1);
+        }
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+        int maior = arrayDesordenado[0];
+        for (int i = 1; i < arrayDesordenado.length; i++){
+            if (arrayDesordenado[i] > maior){
+                maior = arrayDesordenado[i];
+            }
+        }
+        return maior;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
